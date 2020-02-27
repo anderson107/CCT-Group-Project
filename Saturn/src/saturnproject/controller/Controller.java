e2e8ea1d6ceb2c8d6f9f694ed3c4c4b2168d7faa
DELL <DELL@192.168.0.108>
2020-02-27 12:02:43 +0000

    First Commit - Project structure - Registration form

---
+++ Saturn/src/saturnproject/controller/Controller.java
@@ -0,0 +1,12 @@
+package saturnproject.controller;
+
+import javafx.event.Event;
+import javafx.fxml.FXML;
+
+public class Controller {
+
+	@FXML
+	public void buttonClicked(Event e) {
+		System.out.println("Button clicked");
+	}
+}
