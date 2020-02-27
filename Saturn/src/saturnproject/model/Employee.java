b69a624f0c94b10ba53f5ce065fec8f31a0cdfda
Anderson <DELL@192.168.0.108>
2020-02-27 17:29:05 +0000

    Adding package model - class Employee

---
+++ Saturn/src/saturnproject/model/Employee.java
@@ -0,0 +1,17 @@
+package saturnproject.model;
+
+public class Employee {
+
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String DOB;
+	private String mobile;
+	private String telephone;
+	private String address;
+	private String city;
+	private String position;
+	private String password;
+	
+}
