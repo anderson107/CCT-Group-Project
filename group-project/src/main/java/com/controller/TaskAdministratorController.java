c393eb4d00992b4b255a282eee769a6c074065c6
Anderson <DELL@10.250.14.61>
2020-03-12 19:32:15 +0000

    Checklist tableview updated

--- group-project/src/main/java/com/controller/TaskAdministratorController.java
+++ group-project/src/main/java/com/controller/TaskAdministratorController.java
@@ -6,9 +6,12 @@ import java.util.List;
 import java.util.ResourceBundle;
 
 import com.saturn.model.Checklist;
+import com.saturn.model.ChecklistSuperClass;
 import com.saturn.model.FireWarden;
 import com.saturn.model.HealthSafetyChecklist;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
@@ -17,43 +20,56 @@ import javafx.scene.control.cell.PropertyValueFactory;
 public class TaskAdministratorController implements Initializable {
 	
 	@FXML
-	private TableView<Checklist>tableView;
+	private TableView<ChecklistSuperClass>tableView;
 
 	@FXML
-	private TableColumn<Checklist, String>checkbox;
+	private TableColumn<ChecklistSuperClass, String>checkbox;
 	
 	@FXML
-	private TableColumn<Checklist, Integer>id;
+	private TableColumn<ChecklistSuperClass, Integer>id;
 	
 	@FXML
-	private TableColumn<Checklist, String>description;
+	private TableColumn<ChecklistSuperClass, String>description;
 	
 	@FXML
-	private TableColumn<Checklist, String>frequency;
+	private TableColumn<ChecklistSuperClass, String>frequency;
 	
 	@FXML
-	private TableColumn<Checklist, String>status;
+	private TableColumn<ChecklistSuperClass, String>status;
 
+	private List<ChecklistSuperClass> fire;
+	
+	public TaskAdministratorController() {
+		fire = new ArrayList<>();
+		fire.add(new FireWarden("Verifiquei todas a entradas, sem problema nenhum, vamos ficar em  casa", "done", "Daily"));
+		fire.add(new FireWarden("Verificou todas a entradas, sem problema nenhum, vamos ficar em  casa", "done", "Daily"));
+		fire.add(new HealthSafetyChecklist("Verificarei todas a entradas, sem problema nenhum, vamos ficar em  casa", "done", "Daily"));
+	}
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		
-		checkbox.setCellValueFactory(new PropertyValueFactory<Checklist, String>("checkbox"));
-		id.setCellValueFactory(new PropertyValueFactory<Checklist, Integer>("id"));
-		description.setCellValueFactory(new PropertyValueFactory<Checklist, String>("itemDescription"));
-		frequency.setCellValueFactory(new PropertyValueFactory<Checklist, String>("frequency"));
-		status.setCellValueFactory(new PropertyValueFactory<Checklist, String>("status"));
+		checkbox.setCellValueFactory(new PropertyValueFactory<ChecklistSuperClass, String>("checkbox"));
+		id.setCellValueFactory(new PropertyValueFactory<ChecklistSuperClass, Integer>("id"));
+		description.setCellValueFactory(new PropertyValueFactory<ChecklistSuperClass, String>("itemDescription"));
+		frequency.setCellValueFactory(new PropertyValueFactory<ChecklistSuperClass, String>("frequency"));
+		status.setCellValueFactory(new PropertyValueFactory<ChecklistSuperClass, String>("status"));
 
-	    tableView.getItems().setAll(parseUserList());
+	    tableView.getItems().setAll(fire);
 		
 	}
-	
-	private List<Checklist>parseUserList(){
-		List<Checklist>fire = new ArrayList<>();
-		fire.add(new FireWarden("Verificar todas a entradas, sem problema nenhum, vamos ficar em  casa", "done", "Daily"));
-		fire.add(new FireWarden("Verificar todas a entradas, sem problema nenhum, vamos ficar em  casa", "done", "Daily"));
-		fire.add(new HealthSafetyChecklist("Verificar todas a entradas, sem problema nenhum, vamos ficar em  casa", "done", "Daily"));
-		return fire;
 		
+	@FXML
+	private void deleteRow() {
+		ObservableList<ChecklistSuperClass>delete = FXCollections.observableArrayList();
+		for(ChecklistSuperClass c: fire) {
+			if(c.getCheckbox().isSelected()) {
+				delete.add(c);
+			}
+		}
+		fire.removeAll(delete);
+		tableView.getItems().setAll(fire);
+		//tableView.refresh();
 	}
 	
 	 
